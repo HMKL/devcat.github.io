85c4930aa686ea2f834a860b97969a288835c296
HMKL <catalin.bac@gmail.com>
2020-04-30 19:14:51 +0300

    small changes

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -17,25 +17,6 @@ $("#menu-toggle").click(function(e) {
 
 
 
-  var app = document.getElementById('app');
-
-  var typewriter = new Typewriter(app, {
-      loop: true
-  });
-  
-  typewriter.typeString('Hello World!')
-      .pauseFor(2500)
-      .deleteAll()
-      .typeString('Strings can be removed')
-      .pauseFor(2500)
-      .deleteChars(7)
-      .typeString('<strong>altered!</strong>')
-      .pauseFor(2500)
-      .start();
-  
-  
-  
-
 
       // $(() => {
       //   $("#menu-toggle").click(() => {

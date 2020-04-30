d7c29896e8122c78e70604688b5899693ce36dfb
HMKL <catalin.bac@gmail.com>
2020-04-30 18:45:23 +0300

    new files

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -17,3 +17,34 @@ $("#menu-toggle").click(function(e) {
 
 
 
+  var app = document.getElementById('app');
+
+  var typewriter = new Typewriter(app, {
+      loop: true
+  });
+  
+  typewriter.typeString('Hello World!')
+      .pauseFor(2500)
+      .deleteAll()
+      .typeString('Strings can be removed')
+      .pauseFor(2500)
+      .deleteChars(7)
+      .typeString('<strong>altered!</strong>')
+      .pauseFor(2500)
+      .start();
+  
+  
+  
+
+
+      // $(() => {
+      //   $("#menu-toggle").click(() => {
+      //       if ($(this).find('.fa-grip-lines').length > 0) {
+      //           $(this).html("<i class='fas fa-grip-lines-vertical'></i>");
+      //       } else {
+      //           $(this).html("<i class='fas fa-grip-lines'></i>");
+      //       }  
+      //   });
+      // });
+    
+

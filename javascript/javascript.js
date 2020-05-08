6ffec92b6d3c78854b51d0ede0d5c234bd0b940a
HMKL <catalin.bac@gmail.com>
2020-05-08 21:13:03 +0300

    side bar minor changes

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -7,25 +7,22 @@ $("#menu-toggle").click(function(e) {
   // Change icon when button is press
   $(function(){
     $("#menu-toggle").click(function () {
-      if ($(this).find('.fa-grip-lines').length>0) {
-        $(this).html("<i class='fas fa-grip-lines-vertical'></i>");
+      if ($(this).find('.fa-angle-double-left').length>0) {
+        $(this).html("<i class='fas fa-angle-double-right'></i>");
       } else {
-        $(this).html("<i class='fas fa-grip-lines'></i>");
+        $(this).html("<i class='fas fa-angle-double-left'></i>");
       }  
     });
   });
 
+  $('#foo').hide();
 
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 100 && $(this).scrollTop() < 1500) {
+      $('#foo').fadeIn();
+    } else {
+      $('#foo').fadeOut();
+    }
+  });  
 
-
-      // $(() => {
-      //   $("#menu-toggle").click(() => {
-      //       if ($(this).find('.fa-grip-lines').length > 0) {
-      //           $(this).html("<i class='fas fa-grip-lines-vertical'></i>");
-      //       } else {
-      //           $(this).html("<i class='fas fa-grip-lines'></i>");
-      //       }  
-      //   });
-      // });
-    
 
